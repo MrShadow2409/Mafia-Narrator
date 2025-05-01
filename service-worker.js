7421c33f25dbe3c1e89bb8b0fed9e3954058d400
MrShadow2409 <ghdezmontes@gmail.com>
2025-04-30 23:49:55 -0700

    Primer commit - Agregar archivos de la aplicacion

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'mafia-narrador-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',  // Asegúrate de incluir tus archivos CSS y JS
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Si la respuesta está en caché, devolverla; si no, hacer la solicitud normal
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
